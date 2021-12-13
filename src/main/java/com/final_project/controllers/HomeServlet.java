485546e9aaf9d3cbd94732e37d02b77faa9868da
ducphamngoc <19110157@student.hcmute.edu.vn>
2021-12-13 20:35:53 +0700

    replace serlet

--- src/main/java/com/final_project/controllers/HomeServlet.java
+++ src/main/java/com/final_project/controllers/HomeServlet.java
@@ -1,4 +1,19 @@
 package com.final_project.controllers;
 
-public class HomeServlet {
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "HomeServlet", value = "/HomeServlet")
+public class HomeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
 }
