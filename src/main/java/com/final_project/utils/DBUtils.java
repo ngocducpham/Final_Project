02ad7b08b35774458846dc875107f9f1e7feae29
vohuugiau2901 <vohuugiau2901@gmail.com>
2021-12-16 09:22:29 +0700

    .

--- src/main/java/com/final_project/utils/DBUtils.java
+++ src/main/java/com/final_project/utils/DBUtils.java
@@ -4,7 +4,8 @@ import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 
 public class DBUtils {
-    static Sql2o sql2o = new Sql2o("jdbc:mysql://localhost:3306/auctionWeb", "root", "root");
+    static Sql2o sql2o = new Sql2o("jdbc:mysql://localhost:3306/auctionWeb",
+            "root", "root");
 
     public static Connection getConnection() {
         return sql2o.open();
