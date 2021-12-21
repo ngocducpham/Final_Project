e1064beb6a6245c15b99c45635578c970dfa7857
vohuugiau2901 <vohuugiau2901@gmail.com>
2021-12-21 22:13:00 +0700

    search filer

---
+++ src/main/java/com/final_project/Filters/SearchFilter.java
@@ -0,0 +1,24 @@
+package com.final_project.Filters;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter(filterName = "SearchFilter", value = "/Search")
+public class SearchFilter implements Filter {
+    public void init(FilterConfig config) throws ServletException {
+    }
+
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpSession session = request.getSession();
+        session.setAttribute("retUrl", request.getRequestURI()+"?"+request.getQueryString());
+        chain.doFilter(request, response);
+    }
+}
