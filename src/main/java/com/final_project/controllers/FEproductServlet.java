b7c4f205536cb82c68cec045c73eb10f5d6ab357
Thanh <thanhpt21.it@gmail.com>
2021-12-22 16:22:54 +0700

    product detail

--- src/main/java/com/final_project/controllers/FEproductServlet.java
+++ src/main/java/com/final_project/controllers/FEproductServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.annotation.*;
 import java.io.IOException;
 import java.util.List;
 
-@WebServlet(name = "FEproductServlet", value = "/Sanpham/*")
+@WebServlet(name = "FEproductServlet", value = "/Product/*")
 public class FEproductServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
