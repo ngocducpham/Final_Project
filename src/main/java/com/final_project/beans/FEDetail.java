2a987250b6e56b18d2428f0b0908afa596aedb68
CaoPham <19161028@student.hcmute.edu.vn>
2021-12-23 08:04:46 +0700

    beans.FEDettail

--- src/main/java/com/final_project/beans/FEDetail.java
+++ src/main/java/com/final_project/beans/FEDetail.java
@@ -1,6 +1,97 @@
 package com.final_project.beans;
 
+import  java.time.LocalDateTime;
+
 public class FEDetail {
-    private int proID, status,total_bid;
-    private String pname;
+    private int Pro_ID, Status,Total_Bid;
+    private String Pname, username;
+    private LocalDateTime Start_Time, End_Time;
+    private int Price_of_User, max_price;
+
+
+    public FEDetail(String pname, String username, LocalDateTime start_Time, LocalDateTime end_Time,
+        int pro_ID, int price_of_User, int status, int total_Bid, int max_price)
+    {
+        Total_Bid=total_Bid;
+        Pname=pname;
+        this.username=username;
+        Start_Time=start_Time;
+        End_Time=end_Time;
+        Pro_ID=pro_ID;
+        Price_of_User=price_of_User;
+        Status=status;
+        this.max_price=max_price;
+    }
+
+    public LocalDateTime getEnd_Time() {
+        return End_Time;
+    }
+
+    public LocalDateTime getStart_Time() {
+        return Start_Time;
+    }
+
+    public int getPro_ID() {
+        return Pro_ID;
+    }
+
+    public int getMax_price() {
+        return max_price;
+    }
+
+    public int getPrice_of_User() {
+        return Price_of_User;
+    }
+
+    public int getTotal_Bid() {
+        return Total_Bid;
+    }
+
+    public int getStatus() {
+        return Status;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPname() {
+        return Pname;
+    }
+
+    public void setPro_ID(int pro_ID) {
+        Pro_ID = pro_ID;
+    }
+
+    public void setStatus(int status) {
+        Status = status;
+    }
+
+    public void setTotal_Bid(int total_Bid) {
+        Total_Bid = total_Bid;
+    }
+
+    public void setEnd_Time(LocalDateTime end_Time) {
+        End_Time = end_Time;
+    }
+
+    public void setMax_price(int max_price) {
+        this.max_price = max_price;
+    }
+
+    public void setPname(String pname) {
+        Pname = pname;
+    }
+
+    public void setPrice_of_User(int price_of_User) {
+        Price_of_User = price_of_User;
+    }
+
+    public void setStart_Time(LocalDateTime start_Time) {
+        Start_Time = start_Time;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
