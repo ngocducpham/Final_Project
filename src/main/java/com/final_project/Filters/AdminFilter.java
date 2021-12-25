1cb6b47b76cd8c62a49043575b71367dd9086177
vohuugiau2901 <vohuugiau2901@gmail.com>
2021-12-25 21:23:03 +0700

    update security

--- src/main/java/com/final_project/Filters/AdminFilter.java
+++ src/main/java/com/final_project/Filters/AdminFilter.java
@@ -1,8 +1,12 @@
 package com.final_project.Filters;
 
+import com.final_project.beans.User;
+import com.final_project.utils.ServletUtils;
+
 import javax.servlet.*;
 import javax.servlet.annotation.*;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
@@ -18,8 +22,14 @@ public class AdminFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse response, FilterChain chain) throws ServletException, IOException {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpSession session = request.getSession();
-        if(session.getAttribute("adminlogin")==null){
-            session.setAttribute("adminlogin",false);
+        if (session.getAttribute("adminlogin") == null) {
+            session.setAttribute("adminlogin", false);
+        }
+        if (session.getAttribute("authUser") != null) {
+            User u = (User) session.getAttribute("authUser");
+            if (u.getUserrole() < 3) {
+                ServletUtils.forward("/views/404/index.jsp", request,(HttpServletResponse) response);
+            }
         }
         chain.doFilter(request, response);
     }
